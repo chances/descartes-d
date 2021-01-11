4ab5f1b2d3d28a6b6859813c56904ea1560bb010
Chance Snow <git@chancesnow.me>
2021-01-11 08:34:25 -0600

    Add unit tests to the fuzzy equality comparators

--- source/descartes/rough.d
+++ source/descartes/rough.d
@@ -5,7 +5,7 @@
 /// License: MIT License
 module descartes.rough;
 
-import descartes : N, norm, P2, V2;
+import descartes : N, norm, V2;
 import std.math : abs;
 
 /// Thickness radius
@@ -16,11 +16,34 @@ enum double roughTolerance = 0.000_000_1;
 // TODO: How do these compare to [std.math.approxEqual](https://dlang.org/library/std/math/approx_equal.html)?
 
 ///
-bool roughlyEqualTo(N a, N b, N tolerance) {
+bool roughlyEqualTo(N a, N b, N tolerance = roughTolerance) {
   return (a - b).abs <= tolerance;
 }
 
 ///
-bool roughlyEqualTo(V2 a, V2 b, N tolerance) {
+bool roughlyEqualTo(V2 a, V2 b, N tolerance = roughTolerance) {
   return (a - b).norm <= tolerance;
+}
+
+unittest {
+  import descartes : P2;
+
+  assert( P2(10, 20.0).roughlyEqualTo(P2(10, 20.0 + roughTolerance)));
+  assert(!P2(10, 21.0).roughlyEqualTo(P2(10, 20.0 + roughTolerance)));
+  assert( V2(10, 20.0).roughlyEqualTo(V2(10, 20.0 + roughTolerance)));
+  assert(!V2(10, 21.0).roughlyEqualTo(V2(10, 20.0 + roughTolerance)));
+  assert( P2(10, 20.0).roughlyEqualTo(P2(10, 20.0 + (roughTolerance / 2.0f))));
+  assert(!P2(10, 21.0).roughlyEqualTo(P2(10, 20.0 + (roughTolerance / 2.0f))));
+  assert( V2(10, 20.0).roughlyEqualTo(V2(10, 20.0 + (roughTolerance / 2.0f))));
+  assert(!V2(10, 21.0).roughlyEqualTo(V2(10, 20.0 + (roughTolerance / 2.0f))));
+  assert( P2(10, 20.0).roughlyEqualTo(P2(10, 20.0 + (roughTolerance * 5.0f))));
+  assert(!P2(10, 21.0).roughlyEqualTo(P2(10, 20.0 + (roughTolerance * 5.0f))));
+  assert( V2(10, 20.0).roughlyEqualTo(V2(10, 20.0 + (roughTolerance * 5.0f))));
+  assert(!V2(10, 21.0).roughlyEqualTo(V2(10, 20.0 + (roughTolerance * 5.0f))));
+
+  const lowerTolerance = 0.000_1;
+  assert( P2(10, 20.0 + lowerTolerance).roughlyEqualTo(P2(10, 20.000_1), lowerTolerance));
+  assert( V2(10, 20.0 + lowerTolerance).roughlyEqualTo(V2(10, 20.000_1), lowerTolerance));
+  assert(!P2(10, 20.000_000_1).roughlyEqualTo(P2(10, 20.000_1), lowerTolerance));
+  assert(!V2(10, 20.000_000_1).roughlyEqualTo(V2(10, 20.000_1), lowerTolerance));
 }
